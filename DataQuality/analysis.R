a895d916872f5694c82893118b2ff94664af0fd4
mvanegas10 <m.vanegas10@uniandes.edu.co>
2017-09-25 17:35:29 +0200

    Updated requirements.txt and Readme.md

--- DataQuality/analysis.R
+++ DataQuality/analysis.R
@@ -7,13 +7,12 @@ library(caret)
 
 # Read data from CSV file
 data <- read.csv(file="data/cleansed/table_secom.csv",head=FALSE,sep=";")
-print(data)
 
 # Creates control group for Reverse Feature Elimination
 control <- rfeControl(functions=rfFuncs)
 
 # Reverse Feature Elimination
-results <- rfe(data[,1:590], as.factor(data[,591]), sizes=c(5), rfeControl=control)
+results <- rfe(data[,4:526], as.factor(data[,2]), sizes=c(5), rfeControl=control)
 
 predictors(results)
 
@@ -25,6 +24,6 @@ cols$names <- cols.names
 
 #get the 100 names
 
-predictors100 <- cols[1:100,2]
+predictors100 <- cols[1:5,2]
 
 print(predictors100)
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Manufacturing-Process
+
+## Dependencies
+### FrontEnd
+```
+npm install live-server
+```
+
+### BackEnd
+```
+virtualenv venv
+source venv/bin/activate
+pip install -r requirements.txt
+
+```
+
+### DataQuality
+```
+sudo su - -c "R -e \"install.packages('caret', repos = 'http://cran.rstudio.com/')\""
+sudo su - -c "R -e \"install.packages('e1071', repos = 'http://cran.rstudio.com/')\""
+sudo su - -c "R -e \"install.packages('randomForest', repos = 'http://cran.rstudio.com/')\""
+```
